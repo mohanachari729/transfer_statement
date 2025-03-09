a943f2ddf9447f025968093942ffe00e5bb1fa37
MOHANACHARI <ma9861557@gmail.com>
2025-03-09 11:14:53 +0530

    Add files via upload

---
+++ loop_transfer.statement.py
@@ -0,0 +1,41 @@
+# ##Problem 1 : Using break in a While Loop
+# sum = 0
+# number = [25 , 30 , 20 , 40 , 15 , 25 ]
+# for i in number :
+#     if sum + i > 100 :
+#         print(f"{sum} exceeds 100")
+#         break
+#     sum += i
+# print(sum)
+        
+
+# ## Problem 2: Using continue in a For Loop
+# for i in range(1 , 600) :
+#     if i % 2 == 0 :
+#         continue
+#         print(i)     
+#     else :
+#         print(i)
+
+
+# # Problem 3: Using pass in Conditional Statements
+# num = int(input("enter the number :"))
+# if num % 2 == 0 :
+#     print(f"{num} is even")
+# else :
+#     pass
+
+# Problem 4 : Combining Transfer Statements
+list_1 = ["mohan", "hima","latha","srinu"]
+for i in list_1 :
+    if i == "latha" :
+        break
+    if i == "mohan" :
+        continue
+    print(i)
+
+        
+
+
+
+
